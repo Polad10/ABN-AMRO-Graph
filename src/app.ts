fe9692061a31f2897af43014c29e5735b3b820e2
Polad <samadzada.polad@gmail.com>
2024-04-23 17:18:39 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
